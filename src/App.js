df02ca27f77638c9089991d1a03a35a40500fd43
JisunAurnob <jisunabedin@gmail.com>
2023-11-06 19:03:59 +0600

    image gallary design issues fixed

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Image10 from './assets/images/image-10.jpeg';
 import Image11 from './assets/images/image-11.jpeg';
 import { useState } from 'react';
 
+
 function App() {
 
   const [allImages, setAllImages] = useState([
@@ -31,51 +32,89 @@ function App() {
   const [selectedImages, setSelectedImages] = useState([]);
   const [draggedItem, setDraggedItem] = useState(null);
 
+  // Writting a method to store dragged item
   const handleDragStart = (e, item, index) => {
     setDraggedItem({ item: item, index: index });
   };
 
-  const handleDragOver = (e) => {
+  const handleDragOver = (e, targetIndex) => {
     e.preventDefault();
+    // console.log(targetIndex);
+    // if (targetIndex != draggedItem.index) {
+    //   const hoveredElement = document.getElementById(`g-img-${targetIndex}`);
+    //   const draggedElement = document.getElementById(`g-img-${draggedItem.index}`);
+    //   if (hoveredElement && draggedElement) {
+    //     if(targetIndex>draggedItem.index){
+    //       hoveredElement.classList.add('move_left');
+    //       draggedElement.classList.add('move_right');
+    //     }
+    //     else{
+    //       hoveredElement.classList.add('move_right');
+    //       draggedElement.classList.add('move_left');
+    //     }
+    //   }
+    // }
+
   };
+  // const handleDragLeave = (e, targetIndex) => {
+  //   e.preventDefault();
+  //   if (targetIndex != draggedItem.index) {
+  //     // const hoveredElement = document.getElementById(`g-img-${targetIndex}`);
+  //     // const draggedElement = document.getElementById(`g-img-${draggedItem.index}`);
+  //     // if (hoveredElement && draggedElement) {
+  //     //   hoveredElement.classList.remove('move_left');
+  //     //   draggedElement.classList.remove('move_right');
+  //     // }
+  //   }
+  // };
 
   const handleDrop = (e, targetItem, targetIndex) => {
     e.preventDefault();
-    // Make a copy of the original array
+  
+    // Making a copy of the original array
     const reorderedItems = [...allImages];
-    console.log("target index: " + targetIndex + " dragged index: " + draggedItem.index);
+    // console.log("target index: " + targetIndex + " dragged index: " + draggedItem.index);
+
     // Check if the index to replace is within the valid range
     if (targetIndex >= 0 && targetIndex < reorderedItems.length) {
       // const reorderedItems = [...items];
       const [reorderedItem] = reorderedItems.splice(draggedItem.index, 1);
       reorderedItems.splice(targetIndex, 0, reorderedItem);
-
+// Reordering Items using splice
       setAllImages(reorderedItems);
+      // setting the reordered items to all images 
       setDraggedItem(null);
     }
   };
 
   const handleImageSelect = (e) => {
+    // Making a copy of the original array
     let tempArray = [...selectedImages];
     if (e.target.checked === true) {
       tempArray.push(Number(e.target.value))
     } else {
       tempArray.splice(tempArray.indexOf(e.target.value), 1);
     }
+    // If the image is checked then the image's id pushed to the temp array
+    // If the checked is removed then using the splice method removing the element from temparray
     setSelectedImages(tempArray);
+    // Then updating the updated images to setSelectedImages
   }
 
-  const removeSelectedImages = () =>{
-      const filteredData = allImages.filter(item => !selectedImages.includes(item.id));
-      setAllImages(filteredData);
-      setSelectedImages([]);
+  const removeSelectedImages = () => {
+    const filteredData = allImages.filter(item => !selectedImages.includes(item.id));
+    // To remove the selected Images, Filtered the images from selectedImages and stored the remaining images
+    setAllImages(filteredData);
+    // Then againg storing the images to allImages array
+    setSelectedImages([]);
+    // Emptying the selected images
   }
   // console.log(selectedImages);
   return (
     <div className="bg-slate-200">
-
       <div className='flex items-center justify-center'>
         <div className='w-100 lg:w-3/4 my-6 bg-white rounded-lg'>
+          {/* Gallery title section and selected count section */}
           <div className='border-b-2 px-8 py-4'>
             {selectedImages && selectedImages.length > 0 ?
               (<div className='text-bold flex justify-between h-7'>
@@ -85,45 +124,30 @@ function App() {
                 <p className='text-xl font-medium h-7'>Gallery</p>
               )}
           </div>
+          {/* Gallery images grid section */}
           <div className="grid grid-cols-2 md:grid-cols-5 gap-4 p-8">
             {allImages?.map((item, index) => {
               return (
-                index === 0 ? (
-                  <div key={index} className='col-span-2 row-span-2 rounded-lg border gallery_image' onDragStart={(e) => handleDragStart(e, item, index)}
-                    onDragOver={handleDragOver}
-                    onDrop={(e) => handleDrop(e, item, index)}
-                    draggable>
-                    <img className="h-auto max-w-full rounded-lg" src={item.src} alt="" />
-                    <div className='img_hover_overlay'>
-                      <div className='opacity-25 w-full h-full bg-black rounded-lg'>
-                      </div>
-                      <input type='checkbox' className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 absolute top-0 m-4'
-                        name='selected_images[]'
-                        value={item.id}
-                        onChange={(e) => { handleImageSelect(e) }}
-                      />
+                <div key={index} id={`g-img-${index}`} className={`${index === 0 && 'col-span-2 row-span-2'} transition-all border rounded-lg gallery_image`}
+                  onDragStart={(e) => handleDragStart(e, item, index)}
+                  onDragOver={(e) => handleDragOver(e, index)}
+                  // onDragLeave={(e) => handleDragLeave(e, index)}
+                  onDrop={(e) => handleDrop(e, item, index)}
+                  draggable>
+                  <img className="h-auto max-w-full rounded-lg" src={item.src} alt="" />
+                  <div className='img_hover_overlay'>
+                    <div className='opacity-25 w-full h-full bg-black rounded-lg'>
                     </div>
+                    <input type='checkbox' className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 absolute top-0 m-4'
+                      name='selected_images[]'
+                      value={item.id}
+                      onChange={(e) => { handleImageSelect(e) }}
+                    />
                   </div>
-                ) : (
-                  <div key={index} className='transition-all border rounded-lg gallery_image' onDragStart={(e) => handleDragStart(e, item, index)}
-                    onDragOver={handleDragOver}
-                    onDrop={(e) => handleDrop(e, item, index)}
-                    draggable>
-                    <img className="h-auto max-w-full rounded-lg" src={item.src} alt="" />
-                    <div className='img_hover_overlay'>
-                      <div className='opacity-25 w-full h-full bg-black rounded-lg'>
-                      </div>
-                      <input type='checkbox' className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 absolute top-0 m-4'
-                        name='selected_images[]'
-                        value={item.id}
-                        onChange={(e) => { handleImageSelect(e) }}
-                      />
-                    </div>
-                  </div>
-                )
+                </div>
               )
             })}
-            <div className='border-2 border-dashed rounded-lg gallery_image flex items-center justify-center'>
+            <div className='border-2 border-dashed rounded-lg gallery_image flex items-center justify-center min-w-[127px] min-h-[127px]'>
               <div className='content-center'>
                 <center>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
